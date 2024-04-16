c463be4460ed5e7048cc1d9529a9bbc29b3c22ad
“alyssabjavier” <alyssabjavier@gmail.com>
2024-04-16 14:37:35 -0500

    Added length, clear, and entries functions

--- script.js
+++ script.js
@@ -16,10 +16,13 @@ class HashMap {
         this.buckets = new Array(16);
     }
 
-    dropInBucket(Node) {
-        let index = this.hash(Node.key);
-        outOfRangeError(index, this.buckets);
-        this.buckets[index] = Node;
+    handleArraySize() {
+        let currentSize = this.buckets.length;
+        if (this.length / currentSize > 0.75) {
+            this.buckets.length = currentSize * 2;
+        } else {
+            return false;
+        }
     }
 
     // (1)
@@ -36,6 +39,7 @@ class HashMap {
 
     // (2)
     set(key, value) {
+        this.handleArraySize();
         let index = this.hash(key);
         if (!this.buckets[index]) {
             this.buckets[index] = new Node(key, value);
@@ -131,6 +135,57 @@ class HashMap {
             }
         })
         return counter;
+    }    
+
+    // 7
+    clear() {
+        this.buckets.forEach((item, index) => {
+            this.buckets[index] = null; // set array item @ index directly instead of referencing "item"
+        })
+        return this.buckets;
+    }
+
+    // 8
+    keys() {
+        let keys = [];
+        this.buckets.forEach(item => {
+            while (item) {
+                let itemKey = item.key;
+                keys.push(itemKey);
+                item = item.next;
+            }
+        });
+        return keys;
+    }
+
+    // 9
+    values() {
+        let values = [];
+        this.buckets.forEach(item => {
+            while (item) {
+                let itemValue = item.value;
+                values.push(itemValue);
+                item = item.next;
+            }
+        });
+        return values;
+    }
+
+    // 10
+    entries() {
+        let entries = [];
+        this.buckets.forEach(item => {
+            while (item) {
+                let entry = [];
+                let itemKey = item.key;
+                let itemValue = item.value;
+                entry.push(itemKey);
+                entry.push(itemValue);
+                entries.push(entry);
+                item = item.next;
+            }
+        })
+        return entries;
     }
 }
 
@@ -142,6 +197,9 @@ myMap.set('Gian', 'lawyer');
 myMap.set('Yoshi', 'dog');
 myMap.set('Rongel', 'consultant')
 myMap.set('Joy', 'teacher');
+myMap.set('Emmy', 'SE')
+myMap.set('Dane', 'consultant');
+
 myMap.set('Joshlyn', 'analyst');
 myMap.set('Alyssa', 'developer');
 console.log(myMap)
@@ -152,12 +210,19 @@ console.log(myMap);
 console.log(myMap.get('Alyssa'));
 console.log(myMap.get('George'));
 console.log(myMap.get('Clover'));
+console.log(myMap.get('Dane'));
 
 console.log(myMap.has('Alyssa'))
 console.log(myMap.has('Clover'));
 console.log(myMap.has('George'));
 
 console.log(myMap.remove('George'));
+// console.log(myMap.remove('Emmy'));
 // console.log(myMap.remove('Rongel'));
 // console.log(myMap.remove('Clover'))
 console.log(myMap.length());
+console.log(myMap.keys());
+console.log(myMap.values());
+
+console.log(myMap)
+console.log(myMap.entries());
